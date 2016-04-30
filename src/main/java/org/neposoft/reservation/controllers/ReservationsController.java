fcaf58835f3bd4ca3c51432ef3200333b695ffce
Vijay Sapkota <bvsapkota@hotmail.com>
2016-04-30 23:20:37 +0200

    add all http verbs

--- src/main/java/org/neposoft/reservation/controllers/ReservationsController.java
+++ src/main/java/org/neposoft/reservation/controllers/ReservationsController.java
@@ -3,11 +3,17 @@ package org.neposoft.reservation.controllers;
 import domain.AppFacade;
 import domain.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.Set;
 
 
@@ -18,8 +24,45 @@ public class ReservationsController {
     @Autowired
     protected AppFacade facade;
 
+    /**
+     * @api {get} /reservations Returns all restaurants
+     * @apiName GetReservations
+     * @apiGroup Reservation
+     * @apiSuccess {java.util.List} Reservation All reservations in system
+     */
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Set<Reservation> getReservations() {
         return facade.getAllReservations();
+    }
+
+    /**
+     * Add a reservation
+     *
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> putReservtion(@Valid Reservation reservation, BindingResult bindingResult) {
+        reservation.setRestaurant(null);
+
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<String>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        facade.addReservation(reservation);
+
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "{id}")
+    public String updateReservation(@PathVariable String id) {
+
+        return "";
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    public ResponseEntity<String> deleteReservation(@PathVariable Integer id) {
+        facade.deleteReservation(id);
+
+        return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
--- src/main/java/org/neposoft/reservation/controllers/RestaurantsController.java
+++ src/main/java/org/neposoft/reservation/controllers/RestaurantsController.java
@@ -1,24 +1,48 @@
 package org.neposoft.reservation.controllers;
 
+import domain.AppFacade;
+import domain.Restaurant;
 import org.neposoft.reservation.helpers.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.Set;
+
 
 /**
  * Created by mrdezzods on 19/04/16.
  */
 
+/**
+ * @api {get} /restaurants Get all restaurants in the system
+ * @apiName GetRestaurants
+ * @apiGroup Restaurant
+ * @apiSuccess { java.util.List } restaurants All restaurants in the system.
+ */
 @RestController
 @RequestMapping("/restaurants")
 public class RestaurantsController {
 
+    @Autowired
+    protected AppFacade facade;
+
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResponse getAll() {
-        RestTemplate template = new RestTemplate();
-        ApiResponse response = template.getForObject("http://www.eatonline-wip.neposoft.org/api/v1/restaurants/nepalihouse", ApiResponse.class);
-        return response;
+    public Set<Restaurant> getAll() {
+        return facade.getAllRestaurants();
+    }
+
+
+    /**
+     * @api {get} /restaurants/:slug Find a restaurant with specific slug
+     * @apiName findRestaurant
+     * @apiGroup Restaurant
+     * @apiParam {String} slug Slug of the restaurant
+     * @apiSuccess { Restaurant } Restaurant  restaurant with the given slug
+     */
+    public Restaurant find() {
+        return facade.findRestaurantBySlug("");
     }
 }
